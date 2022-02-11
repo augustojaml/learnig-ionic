787548496fea4bc1f08861666f3892dd6d33fe1e
augustojaml <jamonteirolima@gmail.com>
2022-02-11 12:02:00 -0300

    Nelio Alves - FormGroup para controlar o formulário

--- nelioalves/src/app/pages/signup/signup.page.ts
+++ nelioalves/src/app/pages/signup/signup.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -6,9 +7,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.page.scss'],
 })
 export class SignupPage implements OnInit {
-  constructor() {}
+  constructor(private formBuild: FormBuilder) {
+    this.formGroup = this.formBuild.group({
+      name: [
+        'Augusto Monteiro',
+        [
+          Validators.required,
+          Validators.minLength(5),
+          Validators.minLength(10),
+        ],
+      ],
+      email: [
+        'jamonteirolima@gmail.com',
+        [Validators.required, Validators.email],
+      ],
+      type: ['1', [Validators.required]],
+      cpfOrCnpj: [
+        '06134596280',
+        [
+          Validators.required,
+          Validators.minLength(11),
+          Validators.maxLength(14),
+        ],
+      ],
+      password: ['123', [Validators.required]],
+      neighborhood: ['Rua Via', [Validators.required]],
+      number: ['25', [Validators.required]],
+      complement: ['Apto 3', []],
+      district: ['Copacabana', []],
+      zipCode: ['10828333', [Validators.required]],
+      phone1: ['977261827', [Validators.required]],
+      phone2: ['', []],
+      phone3: ['', []],
+      stateId: [null, [Validators.required]],
+      cityId: [null, [Validators.required]],
+    });
+  }
 
-  singUp() {
+  formGroup: FormGroup;
+
+  signupUser() {
     console.log('Enviou o form');
   }
 
